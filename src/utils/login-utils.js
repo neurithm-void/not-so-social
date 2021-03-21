c7fe42184217047073dfb9660e228b0cd6391d41
Junaid Shaikh <junaid.shaikh@quantiphi.com>
2021-03-22 00:00:33 +0530

    loading window close bug

--- src/utils/login-utils.js
+++ src/utils/login-utils.js
@@ -2,10 +2,15 @@
 
 const FB = require("fb");
 const config = require("../../config")();
-const {app, BrowserWindow} = require("electron");
+const {app, BrowserWindow, session} = require("electron");
 const LocalStore = require("../connectors/local-datastore");
 
 
+// useful scopes
+// instagram_basic
+// instagram_content_publish
+// instagram_manage_comments
+// instagram_manage_insights
 
 const facebookAuth = (mainWindow, loadingWindow) =>{
     
@@ -13,7 +18,7 @@ const facebookAuth = (mainWindow, loadingWindow) =>{
 
     let options = {
 		client_id: config.app_id,
-		scopes: 'email',
+		scopes: 'email,instagram_basic,pages_show_list',
 		redirect_uri: 'https://www.facebook.com/connect/login_success.html'
 	};
 
@@ -33,7 +38,7 @@ const facebookAuth = (mainWindow, loadingWindow) =>{
 
 	fbAuthWindow.loadURL(facebookAuthURL);
 	fbAuthWindow.webContents.on('did-finish-load',  () => {
-        loadingWindow.close();
+        // loadingWindow.close();
 		fbAuthWindow.show();
 	});
 
@@ -42,8 +47,6 @@ const facebookAuth = (mainWindow, loadingWindow) =>{
 
     const handleUrl = (url)=>{
 
-        loadingWindow.close();
-
         let raw_code = /access_token=([^&]*)/.exec(url) || null;
 		access_token = (raw_code && raw_code.length > 1) ? raw_code[1] : null;
 		error = /\?error=(.+)$/.exec(url);
@@ -51,24 +54,35 @@ const facebookAuth = (mainWindow, loadingWindow) =>{
 		if (access_token || error) {
 			closedByUser = false;
 			FB.setAccessToken(access_token);
-			FB.api('/me', {
-				fields: ['id', 'name', 'picture.width(800).height(800)']
-			}, function (res) {
+			FB.api('/me', 
+				{fields: ['id', 'name', 'picture.width(800).height(800)', "accounts"]},  
+				(res) => {
+					console.log(res.accounts)
+					
+					datastore.set("name", res.name);
+					datastore.set("user_id", res.id);
+					datastore.set("profile_photo_url", res.picture.data.url);
+					datastore.set("login_status", "in");
 
-				datastore.set("name", res.name);
-				datastore.set("user_id", res.id);
-				datastore.set("profile_photo_url", res.picture.data.url);
-				datastore.set("login_status", "in");
-                let today = new Date();
-                datastore.set("last_loggin", today.toDateString())
+					let today = new Date();
+					datastore.set("last_loggin", today.toDateString())
 
-			});
-			fbAuthWindow.close();
+				});
             mainWindow.show();
 		}
     }
     //capture url once the page navigate to the redirect url
-	fbAuthWindow.webContents.on('will-redirect', (event, url) => handleUrl(url));
+	// fbAuthWindow.webContents.on('will-redirect', (event, url) => handleUrl(url));
+	fbAuthWindow.webContents.on('will-navigate', (event, url) => handleUrl(url));
+	let filter = {
+		urls: [options.redirect_uri + '*']
+	};
+	session.defaultSession.webRequest.onCompleted(filter, (details) => {
+		let url = details.url;
+		loadingWindow.close();
+		handleUrl(url);
+		fbAuthWindow.close();
+	});
 
     //TODO: need to find better way to do this
 	// fbAuthWindow.on('close', () => event.returnValue = closedByUser ? { error: 'The popup window was closed' } : { access_token, error })
