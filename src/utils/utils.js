764420f53dfbd5da38d5db4dbfef45e4697222e6
Junaid Shaikh <junaid.shaikh@quantiphi.com>
2021-03-26 12:09:46 +0530

    date diff

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -20,4 +20,14 @@ class Encryption {
 }
 
 
-module.exports = {Encryption};
+const dateDiffInDays = (date1, date2)=>{
+
+  const diffTime = Math.abs(date2 - date1);
+  const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24)); 
+
+  return diffDays;
+
+}
+
+
+module.exports = {Encryption, dateDiffInDays};
